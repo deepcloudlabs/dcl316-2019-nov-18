a584b5df93620352c6bc21fcb31f446ce9090680
binnur.kurt <binnur.kurt@gmail.com>
2019-11-19 16:04:17 +0300

    Game Statistics Model is developed.

--- game-jquery/js/game.js
+++ game-jquery/js/game.js
@@ -5,20 +5,34 @@ class Move {
     }
 }
 
+class GameStatistics {
+    constructor() {
+        this.wins = 0;
+        this.loses = 0;
+        this.totalWinsTime = 0;
+        this.avgWinsTime = 0;
+        this.totalMoves = 0;
+        this.avgMoves = 0;
+        this.avgMoveTime = 0;
+    }
+}
+
 class GameViewModel {
     constructor() {
         this.secret = this.createSecret();
         this.tries = 0;
         this.moves = [];
+        this.statistics = new GameStatistics();
         this.counter = 30;
     }
 
-    countDown(){
+    countDown() {
         this.counter--;
-        if (this.counter<=0){
-            let move = new Move(this.secret,"Time is out!");
+        if (this.counter <= 0) {
+            let move = new Move(this.secret, "Time is out!");
             this.initGame();
             this.moves.push(move);
+            this.statistics.loses++;
         }
     }
 
@@ -26,8 +40,18 @@ class GameViewModel {
         let move;
         this.tries++;
         if (this.secret == guess) {
+            this.statistics.wins++;
+            this.statistics.totalWinsTime += 30 - this.counter;
+            this.statistics.avgWinsTime =
+                this.statistics.totalWinsTime / this.statistics.wins;
+            this.statistics.totalMoves += this.tries;
+            this.statistics.avgMoves =
+                this.statistics.totalMoves / this.statistics.wins;
+            this.statistics.avgMoveTime =
+                this.statistics.avgWinsTime / this.statistics.avgMoves;
             this.initGame();
             move = new Move(guess, "You win!");
+
         } else {
             if (this.secret > guess) {
                 move = new Move(guess, "Pick larger!");
@@ -37,6 +61,7 @@ class GameViewModel {
             if (this.tries >= 7) {
                 move = new Move(this.secret, "You lose!")
                 this.initGame();
+                this.statistics.loses++;
             }
         }
         this.moves.push(move);
