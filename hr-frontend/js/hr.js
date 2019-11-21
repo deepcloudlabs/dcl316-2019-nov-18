3ed279dc7a81a9ef7ad9452248ff6ed44a17c930
binnur.kurt <binnur.kurt@gmail.com>
2019-11-21 11:24:12 +0300

    FindAll button is active.

--- hr-frontend/js/hr.js
+++ hr-frontend/js/hr.js
@@ -1,5 +1,13 @@
 class HrViewModel {
     constructor(){
         this.employee = new Employee();
+        this.employees = ko.observableArray([]);
+        this.findAll = this.findAll.bind(this);
+    }
+
+    findAll(){
+        fetch(`${AppConfig.REST_API_BASE_URL}/employees?page=0&size=10`)
+            .then( resp => resp.json() )
+            .then( employees => this.employees(employees));
     }
 };
