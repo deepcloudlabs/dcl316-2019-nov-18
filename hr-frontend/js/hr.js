c8053609e8b4225cac91f715f43dc0827f3927ab
binnur.kurt <binnur.kurt@gmail.com>
2019-11-21 11:29:48 +0300

    FindAll: if photo is null then use AppConfig.NO_IMAGE

--- hr-frontend/js/hr.js
+++ hr-frontend/js/hr.js
@@ -8,6 +8,10 @@ class HrViewModel {
     findAll(){
         fetch(`${AppConfig.REST_API_BASE_URL}/employees?page=0&size=10`)
             .then( resp => resp.json() )
+            .then( employees => employees.map( emp => {
+                if (emp.photo == null) emp.photo=AppConfig.NO_IMAGE;
+                return emp;
+            }))
             .then( employees => this.employees(employees));
     }
 };
