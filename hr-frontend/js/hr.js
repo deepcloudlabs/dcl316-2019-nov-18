1cd2ad3874d8a4e842321a94d7e85e6a808fc6c9
binnur.kurt <binnur.kurt@gmail.com>
2019-11-21 13:39:16 +0300

    Photo load and drag-and-drop features are implemented.

--- hr-frontend/js/hr.js
+++ hr-frontend/js/hr.js
@@ -2,18 +2,39 @@ class HrViewModel {
     constructor() {
         this.employee = new Employee();
         this.employees = ko.observableArray([]);
+        this.fileData = ko.observable({
+            dataUrl: ko.observable(AppConfig.NO_IMAGE)
+        });
         this.findAll = this.findAll.bind(this);
         this.find = this.find.bind(this);
+        this.insertFile = this.insertFile.bind(this);
+        this.dragover = this.dragover.bind(this);
+    }
+
+    insertFile(e, data) {
+        e.preventDefault();
+        let files = e.target.files || e.originalEvent.dataTransfer.files;
+        let reader = new FileReader();
+        reader.readAsDataURL(files[0]);
+        reader.onload = (event) => {
+            this.fileData().dataUrl(event.target.result);
+        };
+    }
+
+    dragover(e) {
+        e.preventDefault();
     }
 
     find() {
         fetch(`${AppConfig.REST_API_BASE_URL}/employees/${this.employee.identityNo()}`)
-            .then( resp => resp.json() )
-            .then( employee => {
+            .then(resp => resp.json())
+            .then(employee => {
                 if (employee.photo == null) employee.photo = AppConfig.NO_IMAGE;
                 return employee;
-            }).then( employee => this.employee.refresh(employee));
-
+            }).then(employee => {
+               this.employee.refresh(employee);
+               this.fileData().dataUrl(employee.photo);
+            });
     }
 
     findAll() {
