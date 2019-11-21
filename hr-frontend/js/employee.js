f3bbe4d1ea82da37b52f41324f4a6be4d041a19e
binnur.kurt <binnur.kurt@gmail.com>
2019-11-21 11:46:06 +0300

    Find button is active now.

--- hr-frontend/js/employee.js
+++ hr-frontend/js/employee.js
@@ -1,12 +1,24 @@
 class Employee {
-    constructor(){
-        this.identityNo	= ko.observable("");
+    constructor() {
+        this.identityNo = ko.observable("");
         this.fullname = ko.observable("");
         this.iban = ko.observable("TR");
-        this.salary	= ko.observable(2400);
-        this.department	= ko.observable("IT");
-        this.birthYear	= ko.observable(2000);
-        this.partTime	= ko.observable(false);
-        this.photo	= ko.observable(AppConfig.NO_IMAGE);
+        this.salary = ko.observable(2400);
+        this.department = ko.observable("IT");
+        this.birthYear = ko.observable(2000);
+        this.partTime = ko.observable(false);
+        this.photo = ko.observable(AppConfig.NO_IMAGE);
+        this.refresh = this.refresh.bind(this);
+    }
+
+    refresh(employee) {
+        for (let field in employee) {
+            if (this.hasOwnProperty(field)) {
+                if (ko.isObservable(this[field]))
+                    this[field](employee[field]);
+                else
+                    this[field] = employee[field];
+            }
+        }
     }
 }
--- hr-frontend/js/hr.js
+++ hr-frontend/js/hr.js
@@ -1,17 +1,28 @@
 class HrViewModel {
-    constructor(){
+    constructor() {
         this.employee = new Employee();
         this.employees = ko.observableArray([]);
         this.findAll = this.findAll.bind(this);
+        this.find = this.find.bind(this);
     }
 
-    findAll(){
-        fetch(`${AppConfig.REST_API_BASE_URL}/employees?page=0&size=10`)
+    find() {
+        fetch(`${AppConfig.REST_API_BASE_URL}/employees/${this.employee.identityNo()}`)
             .then( resp => resp.json() )
-            .then( employees => employees.map( emp => {
-                if (emp.photo == null) emp.photo=AppConfig.NO_IMAGE;
+            .then( employee => {
+                if (employee.photo == null) employee.photo = AppConfig.NO_IMAGE;
+                return employee;
+            }).then( employee => this.employee.refresh(employee));
+
+    }
+
+    findAll() {
+        fetch(`${AppConfig.REST_API_BASE_URL}/employees?page=0&size=10`)
+            .then(resp => resp.json())
+            .then(employees => employees.map(emp => {
+                if (emp.photo == null) emp.photo = AppConfig.NO_IMAGE;
                 return emp;
             }))
-            .then( employees => this.employees(employees));
+            .then(employees => this.employees(employees));
     }
 };
